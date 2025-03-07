70d9ce6344c35825ba71806be761a2115808fc58
AmitStredz <ponnan2112@gmail.com>
2025-03-07 16:59:19 +0530

    api format update

--- src/views/login/signupPage.js
+++ src/views/login/signupPage.js
@@ -77,7 +77,7 @@ export default function SignupPage() {
         url: "https://api.brevo.com/v3/smtp/email",
         headers: {
           accept: "application/json",
-          "api-key": process.env.BREVO_API_KEY,
+          "api-key": process.env.REACT_APP_BREVO_API_KEY,
           "content-type": "application/json",
         },
         data: {
