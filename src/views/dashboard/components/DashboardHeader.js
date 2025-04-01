e3c49c68302485447e7640266eae2662aeef516a
AmitStredz <ponnan2112@gmail.com>
2025-04-02 00:20:20 +0530

    dashboardHeader updated

--- src/views/dashboard/components/DashboardHeader.js
+++ src/views/dashboard/components/DashboardHeader.js
@@ -6,6 +6,9 @@ import { TbMoonFilled } from "react-icons/tb";
 
 const DashboardHeader = ({ sideBarIsOpen }) => {
   // const [isOpen, setIsOpen] = useState(false);
+  const [userData, setUserData] = useState(
+    JSON.parse(localStorage.getItem("userData"))
+  );
 
   const { logout } = useAuth();
 
@@ -25,7 +28,7 @@ const DashboardHeader = ({ sideBarIsOpen }) => {
   return (
     <div>
       <div className="border-slate-400 bg-white border-b-[1px] font-aclonica">
-        <header className="flex justify-between items-center mx-6 md:mx-10 p-4 px-2 ">
+        <header className="flex justify-between items-center p-3 sm:p-5 sm:px-10 ">
           <div className="text-xl md:text-2xl inline text-alphaGreen font-semibold">
             <span
               className="cursor-pointer mr-4 text-black"
@@ -33,17 +36,28 @@ const DashboardHeader = ({ sideBarIsOpen }) => {
             >
               {"☰"}
             </span>
-            <h1 className="inline text-green-500">alpha robotics</h1>
+            <h1 className="inline text-green-500 text-[16px] sm:text-[20px]">
+              alpha robotics
+            </h1>
           </div>
-          <div className="hidden md:inline-block text-black">
+          <div className=" text-black">
             <div className="flex justify-between gap-5 items-center font-semibold">
-              
-              <TbMoonFilled className="inline text-3xl cursor-pointer" />
+              {/* <TbMoonFilled className="inline text-3xl cursor-pointer" />
               <FaUserAlt className="inline text-3xl cursor-pointer" />
-              <FaDiscord className="inline text-3xl cursor-pointer" />
-              <span className="mr-7 cursor-pointer border bg-green-500 rounded-lg p-2 px-4" onClick={() => handleLogout()}>
+              <FaDiscord className="inline text-3xl cursor-pointer" /> */}
+              <span
+                className="hidden md:inline-block cursor-pointer border bg-green-500 rounded-lg p-2 px-4"
+                onClick={() => handleLogout()}
+              >
                 Logout
               </span>
+              <div className="flex items-center justify-center gap-2 cursor-pointer rounded-md">
+                <img src="avatar.png" alt="avatar" className="w-8 sm:w-12" />
+
+                <span className="text-[12px] sm:text-[14px]">
+                  {userData?.username}
+                </span>
+              </div>
             </div>
           </div>
         </header>
